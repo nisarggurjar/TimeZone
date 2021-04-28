ac3dd5daeb379e8de88f54d82cabd8ea90682455
nisarggurjar <nisarggurjar16@gmail.com>
2021-04-28 20:00:19 +0530

    Login Logout added

--- customers/urls.py
+++ customers/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('login/', Login, name='login'),
+    path('logout/', Logout, name='logout')
 ]
--- customers/views.py
+++ customers/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User 
+from managment.models import Watch
 
 def Home(request):
-    return render(request, 'index.html')
+    watches = Watch.objects.all()
+    d = {'watches':watches}
+    return render(request, 'index.html', d)
 
 def Login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    error = False
     if request.method == 'POST':
         un = request.POST['username']
         pwd = request.POST['password']
+        print(request.POST)
         user = authenticate(username = un, password = pwd)
-
         if user:
             login(request, user)
-            return  redirect('home')
+            error = False
+            return redirect('home')
         else:
-            print('Authentication failed')
-    return render(request, 'login.html')
+            error = True
+    return render(request, 'login.html', {'error': error})
+
+def Logout(request):
+    logout(request)
+    return redirect('home')
